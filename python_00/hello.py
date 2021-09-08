cbc28727d48b7014a1fb329af2094b13ebd3f5b7
Mamudu <42047513+Mamudu@users.noreply.github.com>
2021-09-08 23:06:11 +0000

    python updated

--- python_00/hello.py
+++ python_00/hello.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+age = int(input("How old are you?"))
+if (age < 19):
+  print("You are in high school")
+elif (age < 12):
+  print ("You are in Middle School")
+else:
+  print("You must be in Elementary School or College")
